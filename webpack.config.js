bcf9ad0da9eea3522497be98bda2fafd317a3543
Fraz Aslam <faslam7646@gmail.com>
2023-02-15 01:36:16 -0500

    added styling, almost done

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
